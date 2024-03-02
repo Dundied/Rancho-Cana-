5d673598e115858257ac56b969830b810a582c30
Dundied <leonardochapa123@outlook.com>
2024-03-02 15:30:47 -0300

    commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const helloworld = () => {
+    return "Hello, World!";
+};
+
+const obj = {
+    prop: {
+        name: "test",
+        info: {
+            youtube: "/leonargat999",
+            url: { value: "" },
+        },
+    },
+};
